0f40f2661418eb7664c8180f0e4a800ef6612504
cleliaaubert <clelia.magically@gmail.com>
2015-10-28 17:54:17 -0200

    comentario maior que não esta habilitando o Commit

---
+++ Descaminhos.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Descaminhos.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestDescaminhos
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
